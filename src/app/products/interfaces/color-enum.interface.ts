4735c3a65ae6f1cc9b84366d7ac44eb8f8e4c3bb
Adrian Molina <amm1088@gmail.com>
2025-03-28 11:23:02 -0400

    LTCC-page-not-found

--- src/app/products/interfaces/color-enum.interface.ts
+++ src/app/products/interfaces/color-enum.interface.ts
@@ -1,10 +1,13 @@
 export enum ColorEnum {
+  AZUL_CLARO = 'Azul Claro',
   AZUL_MARINO = 'Azul Marino',
   AZUL_PETROLEO = 'Azul Petróleo',
   BLANCO = 'Blanco',
+  GUINDO = 'Guindo',
   NEGRO = 'Negro',
   PALO_DE_ROSA = 'Palo de Rosa',
-  PLOMO_OSCURO = 'Plomo Oscuro',
   PLOMO_CLARO = 'Plomo Claro',
-  VERDE = 'Verde',
+  PLOMO_OSCURO = 'Plomo Oscuro',
+  VERDE_CLARO = 'Verde Claro',
+  VERDE_PACAY = 'Verde Pacay',
 }
--- src/app/shared/pages/error404-page/error404-page.component.ts
+++ src/app/shared/pages/error404-page/error404-page.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-error404-page',
   templateUrl: './error404-page.component.html',
-  styleUrl: './error404-page.component.css'
+  styleUrl: './error404-page.component.css',
 })
 export class Error404PageComponent {
+  constructor(private router: Router) {}
 
+  goToHome() {
+    this.router.navigateByUrl('/dashboard');
+  }
 }
